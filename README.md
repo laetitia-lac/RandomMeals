ac16529d755f8ca84b208777cf51e0fbd4728a71
Laetitia <lae.lachat@gmail.com>
2021-02-14 00:28:06 +0100

    add notion of last week recipes

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ conda create --name py36_random_meals_env python=3.6
 conda activate py36_random_meals_env
 pip install -r requirements.txt
 ```
+
+
+## TO DO
+* sort alphabetically shopping list
+* generate requirements.txt
+* find a friendly way to execute main python
+* check unicode characters in pdf
+* generate as google keep [link](https://github.com/kiwiz/gkeepapi)
--- src/main.py
+++ src/main.py
@@ -54,17 +54,37 @@ def write_pdf_file(path_file, chosen_recipes_names, shopping_list):
 
 
 if __name__ == '__main__':
-    nb_of_recipes_to_choose = 3
+    nb_of_recipes_to_choose = 2
 
     logger.debug('Beginning of program')
     list_recipes = return_content_json_file('data/recipes.json')
 
-    # choose the recipe for the week
-    chosen_recipes = random.sample(list_recipes, k=min(nb_of_recipes_to_choose, len(list_recipes)))
-    chosen_recipes_names = [chosen_recipe['name_recipe'] for chosen_recipe in chosen_recipes]
+    # get recipes of last week
+    chosen_recipes_names_last_week = set(return_content_json_file('data/chosen_recipes.json'))
+    logger.debug('Recipes of last week: {}'.format(chosen_recipes_names_last_week))
 
-    # output the chosen recipes of the week
+    # choose the recipes for the week
+    # 1. take recipes which were not selected last week
+    all_recipes_names = set([recipe['name_recipe'] for recipe in list_recipes])
+    unused_recipes_names = all_recipes_names - chosen_recipes_names_last_week
+    logger.debug('Unused recipes: {}'.format(unused_recipes_names))
+    chosen_recipes_names = random.sample(unused_recipes_names,
+                                         k=min(nb_of_recipes_to_choose, len(unused_recipes_names)))
+
+    # 2. if needed, take chosen recipes from last week
+    nb_missing_recipes = nb_of_recipes_to_choose - len(chosen_recipes_names)
+    if nb_missing_recipes > 0:
+        chosen_recipes_names.extend(
+            random.sample(chosen_recipes_names_last_week,
+                          k=min(nb_missing_recipes, len(chosen_recipes_names_last_week))))
+    logger.debug('Recipes of the week: {}'.format(chosen_recipes_names))
+
+    # output the chosen recipes of the week as json
     write_json_file('data/chosen_recipes.json', chosen_recipes_names)
+
+    # get chosen recipes from the name
+    chosen_recipes = [recipe for recipe in list_recipes if recipe['name_recipe'] in chosen_recipes_names]
+    logger.debug('Recipes of the week (with list of ingredients): {}'.format(chosen_recipes))
 
     # generate the shopping list
     shopping_list = dict()
@@ -80,6 +100,10 @@ if __name__ == '__main__':
 
             key = (ingredient_name, ingredient_unit.lower()) if ingredient_unit is not None else ingredient_name
             shopping_list[key] = shopping_list.get(key, 0) + ingredient_quantity
-
     logger.debug('Shopping list: {}'.format(shopping_list))
+
+    # generate the output for user
+    logger.debug('Write output pdf')
     write_pdf_file('data/shopping_list.pdf', chosen_recipes_names, shopping_list)
+
+    logger.debug('End of program')
