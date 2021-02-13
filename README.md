ecc1e6c01c45c85546885375670b68f33bf811f8
Laetitia <lae.lachat@gmail.com>
2021-02-13 22:47:15 +0100

    change format from csv to json

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-RandomMeals
+# RandomMeals
+
+## Getting Started
+
+### Prerequisites
+
+* Python 3.6 (with Anaconda to get project environment).
+
+### Installing
+
+* Create the environment for the project and install the requirements: 
+```
+conda create --name py36_random_meals_env python=3.6
+conda activate py36_random_meals_env
+pip install -r requirements.txt
+```
--- src/main.py
+++ src/main.py
@@ -1,33 +1,44 @@
-import pandas as pd
+import json
+import random
 
 from utils.logger import logger
 
-if __name__ == '__main__':
-    nb_of_recipes_to_choose = 2
 
-    logger.debug('beginning programm')
-    df = pd.read_csv('data/list_recipes.csv', sep=";")
-    logger.debug('columns : ' + df.columns)
+def return_content_json_file(path_file):
+    with open(path_file) as json_file:
+        return json.load(json_file)
+
+
+def write_json_file(path_file, content_json):
+    with open(path_file, 'w') as json_file:
+        json.dump(content_json, json_file)
+
+
+if __name__ == '__main__':
+    nb_of_recipes_to_choose = 3
+
+    logger.debug('Beginning of program')
+    list_recipes = return_content_json_file('data/recipes.json')
 
     # choose the recipe for the week
-    recipes = df['name_recipe'].dropna()
-    chosen_recipes = recipes.sample(nb_of_recipes_to_choose, replace=True)
+    chosen_recipes = random.sample(list_recipes, k=min(nb_of_recipes_to_choose, len(list_recipes)))
+    chosen_recipes_names = [chosen_recipe['name_recipe'] for chosen_recipe in chosen_recipes]
 
-    ## output the chosen recipes of the week
-    chosen_recipes.to_csv('data/list_recipes_of_the_week.csv', index=False, header=False)
+    # output the chosen recipes of the week
+    write_json_file('data/chosen_recipes.json', chosen_recipes_names)
 
     # generate the shopping list
-
     ## get all the ingredients for the week
-    ingredients = list()
-    for index_start_recipe, recipe in chosen_recipes.iteritems():
-        logger.debug("start to select ingredients of this recipe : " + recipe)
-        current_index = index_start_recipe
-        while current_index < len(df.index) and (df.loc[current_index, 'name_recipe'] is not None or df.loc[current_index, 'name_recipe'] == recipe):
-            logger.debug("this ingredient {} for this recipe {} was taken".format(df.loc[current_index, 'name_ingredient'], recipe))
-            ingredients.append(df.loc[current_index, 'name_ingredient'])
-            current_index += 1
-            logger.debug("end to select ingredients of this recipe : " + recipe)
+    shopping_list = dict()
+    for recipe in chosen_recipes:
+        for ingredient_recipe in recipe['ingredients']:
+            ingredient_name = ingredient_recipe['name'].lower()
+            ingredient_quantity = ingredient_recipe['quantity']
 
-    logger.debug("list of ingredients of the week" + str(ingredients))
+            logger.debug('Ingredient "{}" will be added to the shopping list (for recipe "{}")'.format(ingredient_name,
+                                                                                                       recipe[
+                                                                                                           'name_recipe']))
 
+            shopping_list[ingredient_name] = shopping_list.get(ingredient_name, 0) + ingredient_quantity
+
+    logger.debug('Shopping list: {}'.format(shopping_list))
